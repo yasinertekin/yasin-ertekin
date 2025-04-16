62f2d7cea677c963c3d4048ba486f8a3b5f6f928
Yasin Ertekin <yasinertekinn@outlook.com>
2025-04-16 16:02:19 +0300

    Update: Responsive utils changes

--- lib/core/utils/responsive_breakpoints_utils.dart
+++ lib/core/utils/responsive_breakpoints_utils.dart
@@ -7,18 +7,7 @@ final class ResponsiveBreakpointsUtils {
 
   static Widget builder(Widget child) {
     return ResponsiveBreakpoints.builder(
-      child: ResponsiveWrapper.builder(
-        child,
-        maxWidth: 1200,
-        minWidth: 320,
-        defaultScale: true,
-        breakpoints: [
-          const Breakpoint(start: 0, end: 600, name: MOBILE),
-          const Breakpoint(start: 601, end: 800, name: TABLET),
-          const Breakpoint(start: 801, end: 1920, name: DESKTOP),
-          const Breakpoint(start: 1921, end: double.infinity, name: '4K'),
-        ],
-      ),
+      child: child,
       breakpoints: [
         const Breakpoint(start: 0, end: 600, name: MOBILE),
         const Breakpoint(start: 601, end: 800, name: TABLET),
